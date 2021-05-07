c627112ba914581b90b0ecee089f1141f481b9a7
Matheus Diefenthaeler <matheus.axo@gmail.com>
2021-05-07 16:11:21 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,7 +1,9 @@
 import Chart from 'react-apexcharts';
+import { useState } from 'react';
 import axios from 'axios';
 import {BASE_URL} from 'utils/requests';
-import {SaleSum} from 'types/sale'
+import {SaleSum} from 'types/sale';
+import {useEffect} from 'react';
 
     type ChartData = {
         labels: string[];
@@ -9,18 +11,21 @@ import {SaleSum} from 'types/sale'
     }
 
     const DonutChart = () => {
-    //forma errada
-    let chartData : ChartData = {labels: [], series:[]};
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller` )
-        .then(response => {
-        const data = response.data as SaleSum[];
-        const myLabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
 
-        chartData = { labels: myLabels, series: mySeries};
-            console.log(chartData);
-        });
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller` )
+            .then(response => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            setChartData({ labels: myLabels, series: mySeries});
+            });
+    } , []);
+
+
 
 //     const mockData = {
 //         series: [477138, 499928, 444867, 220426, 473088],
